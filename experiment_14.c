cfdc71197cd0a071f3cfc79d48a10412d92699b5
Shivam Kumar Kunwar <1971kunwarshivam@gmail.com>
2025-12-30 23:52:16 +0530

    Update experiment_14.c

--- experiment_14.c
+++ experiment_14.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("You entered:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
