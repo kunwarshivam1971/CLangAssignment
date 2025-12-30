1d96bd7307cc0699a85af50eec038535eef63e80
Shivam Kumar Kunwar <1971kunwarshivam@gmail.com>
2025-12-30 23:42:16 +0530

    Update experiment_4.c

--- experiment_4.c
+++ experiment_4.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    int a, b;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    if (a > b) {
+        printf("Largest = %d\n", a);
+    } else if (b > a) {
+        printf("Largest = %d\n", b);
+    } else {
+        printf("Both numbers are equal\n");
+    }
+
+    return 0;
+}
